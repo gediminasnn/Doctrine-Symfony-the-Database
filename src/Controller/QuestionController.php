b2740a96c2d04e34f4c3a8464652e15324373503
gediminasnn <neniusgediminas@gmail.com>
2020-09-24 18:58:41 +0300

    20 Chapters

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,30 +29,34 @@ class QuestionController extends AbstractController
      */
     public function new(EntityManagerInterface $entityManager)
     {
-        $question = new Question();
-        $question->setName('Missing pants')
-            ->setSlug('missing-pants-'.rand(0,1000))
-            ->setQuestion(<<<EOF
-                Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-                to make my dishes wash themselves. But while I was casting it,
-                I slipped a little and I think `I also hit my pants with the spell`.
-                When I woke up this morning, I caught a quick glimpse of my pants
-                opening the front door and walking out! I've been out all afternoon
-                (with no pants mind you) searching for them.
-                Does anyone have a spell to call your pants back?
-EOF
-            );
-        if (rand(1, 10) > 2) {
-            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-        }
-        $entityManager->persist($question);
-        $entityManager->flush();
+//        $question = new Question();
+//        $question->setName('Missing pants')
+//            ->setSlug('missing-pants-'.rand(0,1000))
+//            ->setQuestion(<<<EOF
+//                Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+//                to make my dishes wash themselves. But while I was casting it,
+//                I slipped a little and I think `I also hit my pants with the spell`.
+//                When I woke up this morning, I caught a quick glimpse of my pants
+//                opening the front door and walking out! I've been out all afternoon
+//                (with no pants mind you) searching for them.
+//                Does anyone have a spell to call your pants back?
+//EOF
+//            );
+//        if (rand(1, 10) > 2) {
+//            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+//        }
+//        $question->setVotes(rand(-20,50));
+//
+//        $entityManager->persist($question);
+//        $entityManager->flush();
 
-        return new Response(sprintf(
-            'Well hallo! The shiny new question is id #%d, slug: %s',
-            $question->getId(),
-            $question->getSlug()
-        ));
+        return new Response('This sounds like a great feature for v2'
+//            sprintf(
+//            'Well hallo! The shiny new question is id #%d, slug: %s',
+//            $question->getId(),
+//            $question->getSlug()
+//        )
+        );
     }
 
     /**
@@ -70,20 +75,20 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
 
-        /** @var \App\Repository\QuestionRepository $repository*/
-        $repository = $entityManager->getRepository(Question::class);
-
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['slug' => $slug]);
-        if(!$question){
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s" ',$slug));
-        }
+//        /** @var \App\Repository\QuestionRepository $repository*/
+//        $repository = $entityManager->getRepository(Question::class);
+//
+//        /** @var Question|null $question */
+//        $question = $repository->findOneBy(['slug' => $slug]);
+//        if(!$question){
+//            throw $this->createNotFoundException(sprintf('no question found for slug "%s" ',$slug));
+//        }
 
 
         $answers = [
@@ -96,6 +101,27 @@ EOF
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers,
+        ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if($direction === 'up'){
+            $question->setVotes($question->getVotes() + 1);
+        }
+        else if ($direction === 'down'){
+            $question->setVotes($question->getVotes() - 1);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug(),
         ]);
     }
 
